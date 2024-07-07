e1c2d5107c43ee88c585dc2b9cdd6c066bd18122
krsna-coder <98330024+Techie-Aayush@users.noreply.github.com>
2024-07-07 23:33:13 +0530

    Update Valid_Parenthesis.cpp

--- Strings/Valid_Parenthesis.cpp
+++ Strings/Valid_Parenthesis.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+bool isValid(string s){
+    stack<char>st;
+
+    for(char &ch: s){
+        if(ch=='('){
+            st.push(')');
+        }
+        else if(ch=='{'){
+            st.push('}');
+        }
+        else if(ch=='['){
+            st.push(']');
+        }
+        else if(st.empty() || st.top()!=ch){
+            //st.empty() means we didnt encounter opening brackets ie: string is }])
+            //st.top()!=ch checking that curr char is not same as top, then return false else pop becuase it is same
+            return false;
+        }
+        else{
+            st.pop();
+            //this means we got matching one pop it
+        }
+    }
+    return st.empty();
+}
+int main(){
+    string s="[](){}";
+    cout<<isValid(s);
+}
+
+// if we opening brace push corresponding close brace and check 
+//Time Complexity: O(N)
